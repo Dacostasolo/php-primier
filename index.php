29d7b0046366938f157ee83a5bb994fa1dec3ea0
Dacosta Solomon <dacsolo10@gmail.com>
2022-05-28 20:34:50 +0000

    added an h3

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     $age = 45;
     echo "<h3> my name is " . $name . "</h3>";
     echo "<h3> and my age is " . $age . "</h3>";
+    echo "<h3> checking github response</h3>"
 
     ?>
     <button type="button" class="btn btn-secondary">click me</button>
